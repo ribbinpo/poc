c6b02f33a3ecb88e2f1ce16e3d949b216a1d8748
ribbinpo <ribbinpo@gmail.com>
2024-04-08 01:38:29 +0700

    feat: :sparkles: implement chat room more

--- nest-typeorm/src/chats/chats.gateway.ts
+++ nest-typeorm/src/chats/chats.gateway.ts
@@ -5,33 +5,47 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
-import { Server } from 'http';
+import { Socket, Server } from 'socket.io';
 
-@WebSocketGateway(81)
+@WebSocketGateway(81, { namespace: '/chat' })
 export class ChatsGateway {
   @WebSocketServer() server: Server;
 
+  // afterInit(): void {
+  //   console.log('Websocket Gateway Initialized.');
+  // }
+
+  handleConnection(client: Socket) {
+    console.log(`New client connected: ${client.id}`);
+  }
+
+  // handleDisconnect(client: Socket) {}
+
   @SubscribeMessage('joinRoom')
   handleJoinRoom(
-    @MessageBody() room: string,
+    @MessageBody() data: { room: string },
     @ConnectedSocket() client: Socket,
-  ): void {
-    client.join(room);
-    client.emit('message', `You joined room: ${room}`);
+  ) {
+    console.log(data.room);
+    client.join(data.room);
+    client.emit('message', `You joined room: ${data.room}`);
   }
 
   @SubscribeMessage('leaveRoom')
-  handleLeaveRoom(@MessageBody() room: string, client: Socket): void {
-    client.leave(room);
-    client.emit('message', `You left room: ${room}`);
+  handleLeaveRoom(
+    @MessageBody() data: { room: string },
+    @ConnectedSocket() client: Socket,
+  ): void {
+    client.leave(data.room);
+    client.emit('message', `You left room: ${data.room}`);
   }
 
   @SubscribeMessage('message')
   handleMessage(
     @MessageBody() data: { room: string; message: string },
-    client: Socket,
+    @ConnectedSocket() client: Socket,
   ): void {
-    client.to(data.room).emit('message', data.message);
+    // client.to(data.room).emit('message', data.message);
+    this.server.to(data.room).emit('message', data.message);
   }
 }
